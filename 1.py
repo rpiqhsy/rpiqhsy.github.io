2f0fcce11b18dac5dadb201a15b104ff508741d3
rpiqhsy <45203022+rpiqhsy@users.noreply.github.com>
2019-09-14 14:08:31 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -2,6 +2,22 @@ from gpiozero import AngularServo
 from time import sleep
 import Adafruit_DHT
 
+servo_pin = 4 #pin of servo
+dht_pin = 16 #pin of dht sensor
+delay = 5 # second,must more than 2. 
+
 servo = AngularServo(servo_pin, min_angle=-90, max_angle=90)
 servo.angle = 0
+
 while True:
+    humi, temp = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11,dht_pin)
+    print("humi = %f ,temp = %f"%(humi,temp))
+    if humi > 80:
+        servo.angle = 90
+        print("servo 90")
+    elif humi < 75:
+        servo angle = 0
+        print("servo 0")
+    else :
+        print("servo err")
+    sleep(delay)
